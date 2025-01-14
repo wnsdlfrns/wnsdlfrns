030e6c0974e21d8f22d0f176ee50a52a54d5133b
wnsdflrns <wnsdflrns@gmail.com>
2025-01-14 22:36:14 +0900

    update

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-## Hi there 👋
+# 주닐의 프로필 😸
 
-<!--
-**wnsdlfrns/wnsdlfrns** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fwnsdlfrns%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hit&edge_flat=false)](https://hits.seeyoufarm.com)
 
-Here are some ideas to get you started:
+![K-Junyyy's GitHub stats](https://github-readme-stats.vercel.app/api?username=wnsdlfrns&show_icons=true&theme=tokyonight)
 
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+[![Solved.ac Profile](http://mazassumnida.wtf/api/generate_badge?boj=jl92)](https://solved.ac/jl92)
+
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=wnsdlfrns&layout=Demo&theme=cobalt)
+
+
+
+## 📕 내 블로그 피드
+<ul><li><a href='https://jl92.tistory.com/9' target='_blank'>1. 애저 - 머신러닝(Azure - Machine Learning; ML)</a></li><li><a href='https://jl92.tistory.com/8' target='_blank'>2. 2025-01-08(수)</a></li><li><a href='https://jl92.tistory.com/7' target='_blank'>3. 2025-01-07(화)</a></li><li><a href='https://jl92.tistory.com/5' target='_blank'>4. Linux의 배경과 주요 구성 요소 그리고 기능</a></li><li><a href='https://jl92.tistory.com/4' target='_blank'>5. 2025-01-06(월)</a></li></ul>
---
+++ index.js
@@ -0,0 +1,52 @@
+import { writeFileSync } from 'node:fs';
+import Parser from "rss-parser";
+
+/**
+ * README.MD에 작성될 페이지 텍스트
+ * @type {string}
+ */
+let text = `# 주닐의 프로필 😸
+
+[![hit](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fwnsdlfrns%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hit&edge_flat=false)](https://hits.seeyoufarm.com)
+
+![wnsdlfrns's GitHub stats](https://github-readme-stats.vercel.app/api?username=wnsdlfrns&show_icons=true&theme=tokyonight)
+
+[![Solved.ac Profile](http://mazassumnida.wtf/api/generate_badge?boj=jl92)](https://solved.ac/jl92)
+
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=wnsdlfrns&layout=demo&theme=cobalt)
+
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=wnsdlfrns&exclude_repo=I_am_Junil,anuraghazra.github.io)
+
+## 📕 내 블로그 피드
+`;
+
+// rss-parser 생성
+const parser = new Parser({
+    headers: {
+        Accept: 'application/rss+xml, application/xml, text/xml; q=0.1',
+    }});
+
+(async () => {
+
+    // 피드 목록
+    const feed = await parser.parseURL('https://jl92.tistory.com/rss'); // 본인의 블로그 주소
+    
+    text += `<ul>`;
+    
+    // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
+    for (let i = 0; i < 5; i++) {
+        const {title, link} = feed.items[i];
+        console.log(`${i + 1}번째 게시물`);
+        console.log(`추가될 제목: ${title}`);
+        console.log(`추가될 링크: ${link}`);
+        text += `<li><a href='${link}' target='_blank'>${i + 1}. ${title}</a></li>`;
+    }
+
+    text += `</ul>`;
+    
+    // README.md 파일 생성
+    writeFileSync('README.md', text, 'utf8', (e) => {
+        console.log(e);
+    })
+    console.log('업데이트 완료');
+})();
