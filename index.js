2a9bd257b8f3be411a77355dfda0b3d447250e33
wnsdflrns <wnsdflrns@gmail.com>
2025-01-18 19:39:29 +0900

    +

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ let text = `# 주닐의 프로필 😸
 
 [![hit](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fwnsdlfrns%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hit&edge_flat=false)](https://hits.seeyoufarm.com)
 
+[<img width="400" src="https://mazassumnida.wtf/api/v2/generate_badge?boj=jl92" title="🪪 solved.ac에서 프로필을 확인해보세요! 🪪">](https://solved.ac/jl92)
 [![Solved.ac Profile](http://mazassumnida.wtf/api/generate_badge?boj=jl92)](https://solved.ac/jl92)
 
 ![wnsdlfrns's GitHub stats](https://github-readme-stats.vercel.app/api?username=wnsdlfrns&show_icons=true&theme=tokyonight)
+
 
 ***
 ## 📕 내 블로그 피드
